43568ece89f361a74b57828182523b5ec8020050
Gilvan Gomes <gilgomes.r@gmail.com>
2017-11-06 11:33:54 -0200

    Re-add files

---
+++ BingTextToSpeech/ControlCommands.cs
@@ -0,0 +1,9 @@
+﻿namespace BingTextToSpeech
+{
+
+    internal class ControlCommands
+    {
+        public static readonly string StartPlayingAudio = "StartPlayingAudio";
+        public static readonly string StopPlayingAudio = "StopPlayingAudio";
+    }
+}
